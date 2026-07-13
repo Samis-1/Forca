ed4d384d373a917191eed9736a0325390ed66453
Samis-1 <samaralidya10@gmail.com>
2026-07-13 13:49:12 -0300

    Desing

--- Scripts/jogo_principal.gd
+++ Scripts/jogo_principal.gd
@@ -1,15 +1,33 @@
 extends Node2D
 
+@onready var label_palavra: Label = $Label
+@onready var caixa_texto: LineEdit = $VBoxContainer/LineEdit
+@onready var botao_enviar: Button = $VBoxContainer/Button
+@onready var sprite_boneco: Sprite2D = $Sprite2D
 
-# Called when the node enters the scene tree for the first time.
+var palavras = [
+	"Destino",
+	"Paralelepípedo",
+	"Amigo",
+	"Gato",
+	"Camila",
+	"Eren",
+	"Alexia",
+	"Samira"
+]
+
+var posicao = randi_range(0,7)
+var palavra_sorteada = palavras[posicao]
+
+var letras_descobertas:Array = []
+var erros: int = 0
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	for i in range(palavra_sorteada.length()):
+		letras_descobertas.append("_")
+	label_palavra.text= " " .join(letras_descobertas)
+	
 func _process(delta: float) -> void:
 	pass
-
-
 func _on_voltar_pressed() -> void:
-	get_tree().change_scene_to_file("res://Prefabs/Menu.tscn")
+		get_tree().change_scene_to_file("res://Prefabs/Menu.tscn")
